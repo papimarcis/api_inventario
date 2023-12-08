e028aeee3dd2eaa3af14da820d394cf5b98c9493
Marcos Grande Aguilella <eldelfinxd@gmail.com>
2023-12-08 23:20:45 +0100

    s

--- inventario/main/views.py
+++ inventario/main/views.py
@@ -3,16 +3,21 @@ from django.http import JsonResponse
 from django.core.files import File
 import os
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home(request):
     res = {'code': 500, 'founded': False, 'response': {}}
     f = open(os.path.join(settings.STATIC_LOG, 'logs.txt'), "a")
     log_file = File(f)
-    try:
-        if request.method == "POST":
-            res = {'code': 200, 'founded': True, 'response': "Hi"}
-    except Exception as e:
-        log_file.write('ERR: (logs) {}\n'.format(str(e)))
+    if request.method == "POST":
+        vrf_token = request.POST.get('vrf_token')
+        if vrf_token == "kkaiidfas":
+            res = {'code': 200, 'response': "Hi"}
+        else:
+            error = "User no auth"
+            res = {'code': 403, 'response': error}
+            log_file.write('ERR: (logs) {}\n'.format(error))
     try:
         log_file.close()
         f.close()
